78757236937bb1738dffe7bd5f6e9d464938b079
Carey Johnson <johnson.careyj@gmail.com>
2018-10-03 20:43:06 -0700

    Added a route to the home page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
           <div className="container">
-            <a className="navbar-brand" href="landing.html">DevConnector</a>
+            <Link className="navbar-brand" to="/">DevConnector</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
               <span className="navbar-toggler-icon"></span>
             </button>
