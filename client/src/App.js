f0b3788c17aa571edc20832cf7c138c5540e7221
Carey Johnson <johnson.careyj@gmail.com>
2018-10-16 23:07:45 -0400

    created reducers folder and store

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'; // Allows for custom routes in the app
 // Browser as Route allow to use the word Router to wrap the function
+import { Provider } from 'react-redux';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -8,20 +10,25 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
 
+
+
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router >
+        </Router >
+      </Provider>
     );
   }
 }
